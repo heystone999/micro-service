64080328d80b4d0deae15b444ef9880ded784458
heystone999 <52831724+heystone999@users.noreply.github.com>
2024-01-26 18:58:18 -0500

    feat(Eureka): service retrieval in order-service

--- cloud-demo/order-service/src/main/java/com/stone/order/OrderApplication.java
+++ cloud-demo/order-service/src/main/java/com/stone/order/OrderApplication.java
@@ -3,6 +3,7 @@ package com.stone.order;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -18,6 +19,7 @@ public class OrderApplication {
      * 创建RestTemplate并注入Spring容器
      */
     @Bean
+    @LoadBalanced
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
--- cloud-demo/order-service/src/main/java/com/stone/order/service/OrderService.java
+++ cloud-demo/order-service/src/main/java/com/stone/order/service/OrderService.java
@@ -19,7 +19,7 @@ public class OrderService {
         // 1.查询订单
         Order order = orderMapper.findById(orderId);
         // 2.利用RestTemplate发送http请求，查询用户
-        String url = "http://localhost:8081/user/" + order.getUserId();
+        String url = "http://userservice/user/" + order.getUserId();
         User user = restTemplate.getForObject(url, User.class);
         // 3.封装user到Order
         order.setUser(user);
