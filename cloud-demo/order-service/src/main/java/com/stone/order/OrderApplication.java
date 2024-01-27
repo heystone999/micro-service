c7472ee5e715effc44fa725d87ced2d8aa3e0b0f
heystone999 <52831724+heystone999@users.noreply.github.com>
2024-01-26 19:31:53 -0500

    feat(Ribbon): Switched load balancing strategy from round-robin to random.

--- cloud-demo/order-service/src/main/java/com/stone/order/OrderApplication.java
+++ cloud-demo/order-service/src/main/java/com/stone/order/OrderApplication.java
@@ -1,5 +1,7 @@
 package com.stone.order;
 
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -22,5 +24,10 @@ public class OrderApplication {
     @LoadBalanced
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public IRule randomRule() {
+        return new RandomRule();
     }
 }
