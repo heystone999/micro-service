105703f2aa8c3c84bbef1b5c76697f3393609997
heystone999 <52831724+heystone999@users.noreply.github.com>
2024-01-28 21:26:48 -0500

    feat(Feign): Customize Feign configuration

--- cloud-demo/order-service/src/main/java/com/stone/order/OrderApplication.java
+++ cloud-demo/order-service/src/main/java/com/stone/order/OrderApplication.java
@@ -2,6 +2,7 @@ package com.stone.order;
 
 import com.netflix.loadbalancer.IRule;
 import com.netflix.loadbalancer.RandomRule;
+import com.stone.order.config.DefaultFeignConfiguration;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,7 +13,7 @@ import org.springframework.web.client.RestTemplate;
 
 @MapperScan("com.stone.order.mapper")
 @SpringBootApplication
-@EnableFeignClients
+@EnableFeignClients(defaultConfiguration = DefaultFeignConfiguration.class)
 public class OrderApplication {
 
     public static void main(String[] args) {
