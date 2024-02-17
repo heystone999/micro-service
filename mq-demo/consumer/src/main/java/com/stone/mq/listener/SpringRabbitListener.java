e8028477ec903368e99b6e9cdca49daddbd71196
heystone999 <52831724+heystone999@users.noreply.github.com>
2024-02-17 16:42:13 -0500

    feat(SpringAMQP): receive message

---
+++ mq-demo/consumer/src/main/java/com/stone/mq/listener/SpringRabbitListener.java
@@ -0,0 +1,12 @@
+package com.stone.mq.listener;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SpringRabbitListener {
+    @RabbitListener(queues = "simple.queue")
+    public void listenSimpleQueue(String msg) {
+        System.out.println("消费者接收到simple.queue的消息: " + msg);
+    }
+}
