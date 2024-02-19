525af05b3477c0291b9f1ed4840b7fe5ec4727ee
heystone999 <52831724+heystone999@users.noreply.github.com>
2024-02-19 18:46:07 -0500

    feat(RestClient): init RestHighLevelClient

---
+++ hotel-demo/src/test/java/com/stone/hotel/HotelIndexTest.java
@@ -0,0 +1,29 @@
+package com.stone.hotel;
+
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+public class HotelIndexTest {
+    private RestHighLevelClient client;
+
+    @Test
+    void testInit() {
+        System.out.println(client);
+    }
+
+    @BeforeEach
+    void setUp() {
+        this.client = new RestHighLevelClient(RestClient.builder(HttpHost.create("http://localhost:9200")));
+    }
+
+    @AfterEach
+    void tearDown() throws IOException {
+        this.client.close();
+    }
+}
