3653c216ee08655255bce3ca473bdafd8f39c160
heystone999 <52831724+heystone999@users.noreply.github.com>
2024-02-20 00:13:48 -0500

    feat(ES-RestClient): Document CRUD

---
+++ hotel-demo/src/test/java/com/stone/hotel/HotelDocumentTest.java
@@ -0,0 +1,84 @@
+package com.stone.hotel;
+
+import com.alibaba.fastjson.JSON;
+import com.stone.hotel.pojo.Hotel;
+import com.stone.hotel.pojo.HotelDoc;
+import com.stone.hotel.service.IHotelService;
+import org.apache.http.HttpHost;
+import org.elasticsearch.action.delete.DeleteRequest;
+import org.elasticsearch.action.get.GetRequest;
+import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.update.UpdateRequest;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.xcontent.XContentType;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.IOException;
+
+@SpringBootTest
+public class HotelDocumentTest {
+    @Autowired
+    private IHotelService hotelService;
+    private RestHighLevelClient client;
+
+
+    @Test
+    void testAddDocument() throws IOException {
+        Hotel hotel = hotelService.getById(61083L);
+        HotelDoc hotelDoc = new HotelDoc(hotel);
+        // 准备Request对象
+        IndexRequest request = new IndexRequest("hotel").id(hotelDoc.getId().toString());
+        // 准备JSON文档
+        request.source(JSON.toJSONString(hotelDoc), XContentType.JSON);
+        // 发送请求
+        client.index(request, RequestOptions.DEFAULT);
+    }
+
+    @Test
+    void testGetDocumentById() throws IOException {
+        // 准备Request
+        GetRequest request = new GetRequest("hotel", "61083");
+        // 发送请求
+        GetResponse response = client.get(request, RequestOptions.DEFAULT);
+        // 解析响应结果
+        String json = response.getSourceAsString();
+
+        HotelDoc hotelDoc = JSON.parseObject(json, HotelDoc.class);
+        System.out.println(hotelDoc);
+    }
+
+    @Test
+    void testUpdateDocument() throws IOException {
+        // 创建Request对象
+        UpdateRequest request = new UpdateRequest("hotel", "61083");
+        // 准备参数
+        request.doc("price", "952", "starName", "四钻");
+        // 发送请求
+        client.update(request, RequestOptions.DEFAULT);
+    }
+
+    @Test
+    void testDeleteDocument() throws IOException {
+        // 准备Request
+        DeleteRequest request = new DeleteRequest("hotel", "61083");
+        // 发送请求
+        client.delete(request, RequestOptions.DEFAULT);
+    }
+
+    @BeforeEach
+    void setUp() {
+        this.client = new RestHighLevelClient(RestClient.builder(HttpHost.create("http://localhost:9200")));
+    }
+
+    @AfterEach
+    void tearDown() throws IOException {
+        this.client.close();
+    }
+}
