50a7f850515ba4d8561489620de4e3309997d776
heystone999 <52831724+heystone999@users.noreply.github.com>
2024-02-19 19:39:17 -0500

    feat(ES-RestClient): Get/Delete Index

--- hotel-demo/src/test/java/com/stone/hotel/HotelIndexTest.java
+++ hotel-demo/src/test/java/com/stone/hotel/HotelIndexTest.java
@@ -1,10 +1,12 @@
 package com.stone.hotel;
 
 import org.apache.http.HttpHost;
+import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.client.indices.CreateIndexRequest;
+import org.elasticsearch.client.indices.GetIndexRequest;
 import org.elasticsearch.xcontent.XContentType;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -30,6 +32,24 @@ public class HotelIndexTest {
         request.source(MAPPING_TEMPLATE, XContentType.JSON);
         // 发送请求
         client.indices().create(request, RequestOptions.DEFAULT);
+    }
+
+    @Test
+    void testDeleteHotelIndex() throws IOException {
+        // 创建Request对象
+        DeleteIndexRequest request = new DeleteIndexRequest("hotel");
+        // 发送请求
+        client.indices().delete(request, RequestOptions.DEFAULT);
+    }
+
+    @Test
+    void testExistsHotelIndex() throws IOException {
+        // 创建Request对象
+        GetIndexRequest request = new GetIndexRequest("hotel");
+        // 准备请求参数: DSL语句
+        // 发送请求
+        boolean exists = client.indices().exists(request, RequestOptions.DEFAULT);
+        System.err.println(exists ? "索引库已经存在" : "索引库不存在");
     }
 
     @BeforeEach
