41521fc2aad36af2c524ca05fb7d0beb6b48e122
heystone999 <52831724+heystone999@users.noreply.github.com>
2024-02-23 04:40:12 -0500

    feat(data synchronization): send MQ messages

--- hotel-admin/src/main/java/com/stone/hotel/web/HotelController.java
+++ hotel-admin/src/main/java/com/stone/hotel/web/HotelController.java
@@ -2,9 +2,11 @@ package com.stone.hotel.web;
 
 
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.stone.hotel.constants.MqConstants;
 import com.stone.hotel.pojo.Hotel;
 import com.stone.hotel.pojo.PageResult;
 import com.stone.hotel.service.IHotelService;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,6 +18,8 @@ public class HotelController {
 
     @Autowired
     private IHotelService hotelService;
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
 
     @GetMapping("/{id}")
     public Hotel queryById(@PathVariable("id") Long id) {
@@ -23,10 +27,7 @@ public class HotelController {
     }
 
     @GetMapping("/list")
-    public PageResult hotelList(
-            @RequestParam(value = "page", defaultValue = "1") Integer page,
-            @RequestParam(value = "size", defaultValue = "1") Integer size
-    ) {
+    public PageResult hotelList(@RequestParam(value = "page", defaultValue = "1") Integer page, @RequestParam(value = "size", defaultValue = "1") Integer size) {
         Page<Hotel> result = hotelService.page(new Page<>(page, size));
 
         return new PageResult(result.getTotal(), result.getRecords());
@@ -35,6 +36,8 @@ public class HotelController {
     @PostMapping
     public void saveHotel(@RequestBody Hotel hotel) {
         hotelService.save(hotel);
+
+        rabbitTemplate.convertAndSend(MqConstants.HOTEL_EXCHANGE, MqConstants.HOTEL_INSERT_KEY, hotel.getId());
     }
 
     @PutMapping()
@@ -43,10 +46,14 @@ public class HotelController {
             throw new InvalidParameterException("id不能为空");
         }
         hotelService.updateById(hotel);
+
+        rabbitTemplate.convertAndSend(MqConstants.HOTEL_EXCHANGE, MqConstants.HOTEL_INSERT_KEY, hotel.getId());
     }
 
     @DeleteMapping("/{id}")
     public void deleteById(@PathVariable("id") Long id) {
         hotelService.removeById(id);
+
+        rabbitTemplate.convertAndSend(MqConstants.HOTEL_EXCHANGE, MqConstants.HOTEL_DELETE_KEY, id);
     }
 }
