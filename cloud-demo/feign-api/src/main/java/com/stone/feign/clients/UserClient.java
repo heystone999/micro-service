1ed29d7108ef5301ea05798832bd5aa3d0b09cec
heystone999 <52831724+heystone999@users.noreply.github.com>
2024-02-26 18:13:37 -0500

    feat(Sentinel): Integrate Feign with Sentinel

--- cloud-demo/feign-api/src/main/java/com/stone/feign/clients/UserClient.java
+++ cloud-demo/feign-api/src/main/java/com/stone/feign/clients/UserClient.java
@@ -1,11 +1,12 @@
 package com.stone.feign.clients;
 
+import com.stone.feign.clients.fallback.UserClientFallbackFactory;
 import com.stone.feign.pojo.User;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient("userservice")
+@FeignClient(value = "userservice", fallbackFactory = UserClientFallbackFactory.class)
 public interface UserClient {
     @GetMapping("/user/{id}")
     User findById(@PathVariable("id") Long id);
--- cloud-demo/feign-api/src/main/java/com/stone/feign/config/DefaultFeignConfiguration.java
+++ cloud-demo/feign-api/src/main/java/com/stone/feign/config/DefaultFeignConfiguration.java
@@ -1,5 +1,6 @@
 package com.stone.feign.config;
 
+import com.stone.feign.clients.fallback.UserClientFallbackFactory;
 import feign.Logger;
 import org.springframework.context.annotation.Bean;
 
@@ -8,5 +9,10 @@ public class DefaultFeignConfiguration {
     @Bean
     public Logger.Level logLevel() {
         return Logger.Level.BASIC;
+    }
+
+    @Bean
+    public UserClientFallbackFactory userClientFallbackFactory() {
+        return new UserClientFallbackFactory();
     }
 }
